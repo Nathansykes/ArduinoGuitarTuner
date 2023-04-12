a5ef31b4dc2581a9840b5648af93acf163656864
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2023-04-12 18:39:04 +0100

    removing notes not on a guitar

--- AutomaticGuitarTuner/AutomaticGuitarTuner.ino
+++ AutomaticGuitarTuner/AutomaticGuitarTuner.ino
@@ -3,16 +3,6 @@
 #include <string.h>
 
 #pragma region Notes
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
 #define NOTE_A1  55
 #define NOTE_AS1 58
 #define NOTE_B1  62
@@ -69,29 +59,6 @@
 #define NOTE_D6  1175
 #define NOTE_DS6 1245
 #define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
 #pragma endregion
 
 #define SAMPLES 128
